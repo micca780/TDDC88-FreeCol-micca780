cf441f6ff4f0fa1013279582fbd60d3d34ec1f44
Michelle HC <micca780@student.liu.se>
2018-10-01 18:13:09 +0200

    commit change

--- test/src/net/sf/freecol/common/sound/SoundTest.java
+++ test/src/net/sf/freecol/common/sound/SoundTest.java
@@ -79,7 +79,7 @@ public class SoundTest extends FreeColTestCase {
   //      }
   //  }
 
-
+//
     public void testSound() {
         // these sounds are base resources, and should be enough for a test
         playSound("sound.intro.general");
